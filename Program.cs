55568801cda7d041ef848998f6180bc4c4074680
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-03-25 17:51:26 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// to nervoso
+
+string Frase;
+string FraseToNervoso;
+string Usuario;
+
+
+Console.Write("Digite seu Nome:");
+Usuario = Console.ReadLine()!; 
+
+Console.Write($"{Usuario}, o que você acha de mim ?");
+Frase = Console.ReadLine()!;
+
+FraseToNervoso = $"{Frase.Remove(10)}... NÂO, PERA. \nTô nervoso";
+
+Console.WriteLine(FraseToNervoso);
